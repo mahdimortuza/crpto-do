4e16d1e29737c6bfebb6754c7c3eb89ecda06c5c
mahdimortuza <mahdi.webx@gmail.com>
2024-07-15 14:00:01 +0600

    navbar added

---
+++ script/main.js
@@ -0,0 +1,11 @@
+// logic for handling toggle menu
+    $(document).ready(function() {
+        $('#menu-toggle').click(function() {
+            $('#mobile-menu').toggleClass('hidden');
+        });
+        $('[id="menu-item"]').click(function() {
+            $('#mobile-menu').addClass('hidden');
+
+        })
+    }); 
+
