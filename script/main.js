4df6bcf658cc9ad54262d7a1ea438a588494467b
mahdimortuza <mahdi.webx@gmail.com>
2024-07-16 20:39:23 +0600

    slider added

--- script/main.js
+++ script/main.js
@@ -9,4 +9,24 @@
         })
     }); 
 
- 
+//  slider logic starts here 
+   var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      freeMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {        
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+      }
+    });
