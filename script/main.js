28541fd1710420bb161c96c4ab96543a521ff2e6
mahdimortuza <mahdi.webx@gmail.com>
2024-07-16 12:08:16 +0600

    slider section

--- script/main.js
+++ script/main.js
@@ -9,3 +9,30 @@
         })
     }); 
 
+
+    // logic for team  slider
+    $(document).ready(function () {
+        // get card width dynamically
+        cardWidth = $(".slide-card").width();
+        // next-arrow
+        $('#next-arrow').click(function () {
+            var firstChildAppend = $(".slide-card:first-child()");
+            $(".slide-card").animate({ left: -cardWidth }, function () {
+                $('.slider-wrap').append(firstChildAppend);
+                $(".slide-card").css({
+                    left: 0,
+                })
+            })
+        });
+        // previous-arrow
+        $('#previous-arrow').click(function () {
+            var lastChildPrepend = $(".slide-card:last-child()");
+            $(".slide-card").animate({ left: cardWidth }, function () {
+                $('.slider-wrap').prepend(lastChildPrepend);
+                $(".slide-card").css({
+                    left: 0,
+                })
+            })
+        });
+    });
+    
